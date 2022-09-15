26b84646a53378c9bc194bce85999363698031af
seansnediker <94629278+seansnediker@users.noreply.github.com>
2022-09-14 23:59:19 -0500

    Update sketch.js

--- Unit1/empty-example/sketch.js
+++ Unit1/empty-example/sketch.js
@@ -23,29 +23,23 @@ function draw() {
 
   if (mouseIsPressed) {
     
-    fill("#ECF48A");
+    fill(255,0 ,0);
     ellipse(701, 73, 100, 100);
     
-    fill("#E8DA06");
+    fill(102, 0, 0);
     ellipse(725, 47, 10, 8);
     ellipse(670, 70, 8, 12);
     ellipse(713, 99, 15, 20);
     ellipse(683, 100, 8, 7);
     ellipse(700, 65, 8, 7);
     
-    fill("brown");
-    rect(82, 200, 50, 240);
+    fill("#151617");
+    rect(82, 200, 50, 240);    
     
-    fill("green");
-    triangle(30, 220, 103, 127, 185, 220);
-    triangle(50, 165, 160, 165, 103, 100);
-    triangle(66, 125, 140, 125, 103, 70);
-    
-    fill("brown");
-    ellipse(76, 193, 10, 15);
-    ellipse(121, 143, 10, 15);
-    ellipse(135, 202, 10, 15);
-    ellipse(99, 99, 10, 15);
+    fill("orange");
+    triangle(82, 200, 99, 200, 87, 179);
+    triangle(99, 200, 114, 200, 106, 167);
+    triangle(114, 200, 131, 200, 123, 163);
     
     fill("white");
     ellipse(387, 335, 150, 150);
@@ -62,21 +56,29 @@ function draw() {
     rect(363, 80, 50, 30);
 
     stroke(16);
-    line(252, 223, 323, 263);
+    line(448, 223, 534, 180);
     line(452, 263, 522, 230);
     line(522, 230, 531, 215);
     line(522, 230, 536, 227);
-    line(275, 235, 282, 224);
-    line(260, 227, 242, 227);
+    line(534, 180, 533, 167);
+    line(534, 180, 543, 172);
+    
 
     fill("orange");
     triangle(408, 159, 385, 149, 385, 161);
 
     fill("#484654");
-    arc(385, 180, 30, 30, 0, PI + QUARTER_PI, CHORD);
-
+    stroke(16);
+    line(374, 175, 393, 180)
+    line(360, 125, 375, 136)
+    line(405, 135, 418, 136)
+   
     fill("white");
     rect(0, 440, 1600, 730);
+    
+    stroke(16);
+    line(329, 383, 300, 450);
+    line(450, 380, 475, 450);
   } else {
     // when the mouse isn't pressed!
 
