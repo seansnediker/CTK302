916e7ba83890ffb4b66270990e9be72a0c875f35
seansnediker <sgsnedi@ilstu.edu>
2022-12-11 19:03:34 -0600

    need egg big

--- Unit4/empty-example/sketch.js
+++ Unit4/empty-example/sketch.js
@@ -1,7 +1,41 @@
+// from https://creative-coding.decontextualize.com/video/
+
+var k;
+var g;
+var b;
+var a;
+var cap;
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(1080, 720);
+  cap = createCapture(VIDEO);
+  cap.hide();
+  ellipseMode(CENTER);
+  rectMode(CENTER);
+  imageMode(CENTER);
+  noStroke();
 }
-
 function draw() {
+  background("black");
+  r = (255); // r is a random number between 0 - 255
+  g = (200); // g is a random number betwen 100 - 200
+  b = (100); // b is a random number between 0 - 100
+  cap.loadPixels();
+  
+  for (var cy = 0; cy < cap.height; cy += 4) {
+    for (var cx = 0; cx < cap.width; cx += 3) {
+      var offset = ((cy*cap.width)+cx)*4;
+      var xpos = (cx / cap.width) * width;
+      var ypos = (cy / cap.height) * height;
+      fill(r, g, b);
+      ellipse(xpos, ypos, 5,
+       5 * (cap.pixels[offset+200]/455));
+      
+      
+    }
+  }
+  
+  
 
+ // image(cap, mouseX, mouseY, 160, 120);
+  
 }
